f1524ea5df2e7341bf478f84b3d402f362c4c6b6
jupiterzhuo <jupiterc@gmail.com>
2020-03-12 09:56:29 +0700

    Testing Git

--- day03/spring-restful-example/src/main/java/com/sciencom/springrestfulexample/service/UserService.java
+++ day03/spring-restful-example/src/main/java/com/sciencom/springrestfulexample/service/UserService.java
@@ -1,6 +1,10 @@
 package com.sciencom.springrestfulexample.service;
 
-public class UserService {
+import com.sciencom.springrestfulexample.request.UserRequest;
+import com.sciencom.springrestfulexample.response.UserResponse;
 
-	
+public interface UserService {
+	//Save
+	UserResponse saveUser(UserRequest userRequest);
+	//ListAll
 }
